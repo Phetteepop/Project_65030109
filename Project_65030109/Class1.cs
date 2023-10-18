0cc7c15cdd92e3894b2aaa7db5c80d8b7178f354
admin <admin@1602-39>
2023-10-18 10:07:42 +0700

    Add project files.

---
+++ Project_65030109/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_65030109
+{
+    public class Project
+    {
+        public enum Members
+        {
+            Phet = 1,
+            Mag = 2,
+            Kla = 3,
+        }
+    }
+}
